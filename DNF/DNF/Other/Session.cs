55b3efd22efac7c921476793f9e34a1433f09890
del19@wp.pl <del19@wp.pl>
2014-01-29 00:32:17 +0100

    Init. Logowanie

---
+++ DNF/DNF/Other/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNF.Other
+{
+    public static class Session
+    {
+        public static DNF.users zalogowanyUzytkownik;
+
+
+    }
+}
---
+++ DNF/DNF/Panele/Logowanie.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DNF.Panele
+{
+    /// <summary>
+    /// Interaction logic for Logowanie.xaml
+    /// </summary>
+    public partial class Logowanie : Window
+    {
+        public Logowanie()
+        {
+            InitializeComponent();
+        }
+
+        private void zaloguj(object sender, RoutedEventArgs e)
+        {
+            using (var dc = new DNFEntities())
+            {
+                var user = (from us in dc.users where us.Login == loginTb.Text && us.Haslo == hasloTb.Password select us).ToList();
+                if (user != null && user.Count() > 0)
+                {
+                    Other.Session.zalogowanyUzytkownik = user.First();
+
+                }
+                else
+                    MessageBox.Show("Dane logowania nieprawidlowe");
+            }
+        }
+    }
+}
