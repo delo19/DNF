c73c28d613072986e52e18f71d0e1031ac63a64d
del19@wp.pl <del19@wp.pl>
2014-01-29 23:56:21 +0100

    Dok

--- DNF/UnitTests/UnitTest1.cs
+++ DNF/UnitTests/UnitTest1.cs
@@ -13,20 +13,20 @@ namespace UnitTests
             var myUsers= ManagerLogic.Zaloguj("elo", "elo");
             Assert.AreEqual(myUsers.Count,1);
 
-            //var myUsers1 = ManagerLogic.Zaloguj("user", "user");
-            //Assert.AreEqual(myUsers1.Count, 1);
+            var myUsers1 = ManagerLogic.Zaloguj("user", "user");
+            Assert.AreEqual(myUsers1.Count, 1);
 
-            //var myUsers2 = ManagerLogic.Zaloguj("prow1", "prow1");
-            //Assert.AreEqual(myUsers2.Count, 1);
+            var myUsers2 = ManagerLogic.Zaloguj("prow1", "prow1");
+            Assert.AreEqual(myUsers2.Count, 1);
 
-            //var myUsers3 = ManagerLogic.Zaloguj("user", "user");
-            //Assert.AreEqual(myUsers3.Count, 1);
+            var myUsers3 = ManagerLogic.Zaloguj("user", "user");
+            Assert.AreEqual(myUsers3.Count, 1);
 
-            //var myUsers4 = ManagerLogic.Zaloguj("adam12", "adam12");
-            //Assert.AreEqual(myUsers4.Count, 1);
+            var myUsers4 = ManagerLogic.Zaloguj("adam12", "adam12");
+            Assert.AreEqual(myUsers4.Count, 1);
 
-            //var myUsers5 = ManagerLogic.Zaloguj("adam", "adam");
-            //Assert.AreEqual(myUsers5.Count, 1);
+            var myUsers5 = ManagerLogic.Zaloguj("adam", "adam");
+            Assert.AreEqual(myUsers5.Count, 1);
 
 
         }
@@ -34,8 +34,8 @@ namespace UnitTests
         [TestMethod]
         public void DajWydarzeniaTest()
         {
-            //var myEvents= ManagerLogic.DajWydarzenia();
-            //Assert.AreEqual(myEvents.Count, 0);
+            var myEvents= ManagerLogic.DajWydarzenia();
+            Assert.AreEqual(myEvents.Count, 0);
 
             var myEvents2 = ManagerLogic.DajWydarzenia();
             Assert.AreEqual(0, 0);
